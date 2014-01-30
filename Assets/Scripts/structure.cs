95c0d7c910b3b508b74253986a100d2b709ba625
horio <koppepan0114@gmail.com>
2014-01-30 16:56:42 +0900

    ソース追加
    構造体、列挙体を1つのソースにまとめる

---
+++ Assets/Scripts/structure.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// パズル遷移
+public enum PuzzleState
+{
+	NONE,
+	SELECT,		// ピースの選択
+	MOVE,		// ピースの移動
+	JUDGE,		// ピース消すかの判定
+	DEATH,		// ピースを消す
+	DOWN,		// 隙間を埋める
+	END
+};
+
+// マウスデータ
+public struct MouseData
+{
+	public bool down;
+	public bool up;
+	public Vector3 pos;
+};
+
+// ピースの色
+public enum PieceColor
+{
+	NONE = 0,
+	Red,
+	Blue,
+	Green,
+	Yellow,
+	Purple,
+	Heart
+};
+
+// ピースがそろっている方向
+public enum PieceDirction
+{
+	HEIGHT = 0,
+	WIDTH
+};
+
+// ピースの座標
+public struct PiecePos
+{
+	public int x;
+	public int y;
+
+	public PiecePos(int i, int j)
+	{
+		x = i;
+		y = j;
+	}
+};
